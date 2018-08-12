9f6a513c2d34de04012ffab53a7e538eb9e95ce0
ryeon <ryeonnaderi@gmail.com>
2018-08-12 15:48:02 -0400

    got it to work

--- server.js
+++ server.js
@@ -1,35 +1,58 @@
 // requiring dependencies
 
 var express = require("express");
-// var mongojs = require("mongojs");
+var mongojs = require("mongojs");
 var request = require("request");
 var cheerio = require("cheerio");
+
 // Initializing Express
 var app = express();
 var URL = "https://en.wikipedia.org/wiki/Africa#Economy";
+
 // database config 
-// var databaseUrl = "scrapper";
-// var collections = ["scrappedData"];
+var databaseUrl = "scrapper";
+var collections = ["scrappedData"];
+var db = mongojs(databaseUrl, collections);
 
-request(URL, function (err, res, body) {
-    var $ = cheerio.load(body);
-    var result = [];
+db.on("error", function (error) {
+    console.log("database error: ", error);
+});
 
-    $("tbody").each(function (i, element) {
+app.get("/", function (req, res) {
+    res.send("Hello world");
+});
+app.get("/get", function (req, res) {
+    request(URL, function (err, res, body) {
+        var $ = cheerio.load(body);
+        var result = [];
 
-        var link = $(element).children().attr("href");
-        var title = $(element).children().text();
+        $("tbody").each(function (i, element) {
 
-        result.push({
-            title: title,
-            link: link
+            var link = $(element).children().attr("href");
+            var title = $(element).children().text();
+
+            result.push({
+                title: title,
+                link: link
+            });
+
         });
-
+        // console.log(result);
+        db.scrapper.insert({
+            result
+        });
+        res.toJSON(result);
     });
-    console.log(result);
+});
+app.get("/find", function (req, res) {
+
+    db.scrapper.find({}, function (err, data) {
+        console.log(res);
+        res.json(data);
+    });
 });
 
 
-// app.listen(8080, function () {
-//     console.log("app is running 8080");
-// });
+app.listen(8080, function () {
+    console.log("app is running 8080");
+});
